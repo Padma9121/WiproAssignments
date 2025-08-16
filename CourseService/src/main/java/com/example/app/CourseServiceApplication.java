e913277274535f5fa1d017cbd8a886d4b2b17dad
padmamala9121@gmail.com <you@example.com>
2025-08-16 22:30:24 +0530

    Initial commit - Student Application

---
+++ CourseService/src/main/java/com/example/app/CourseServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Enable
+@SpringBootApplication
+public class CourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseServiceApplication.class, args);
+	}
+
+}
---
+++ E-Learing-App/src/main/java/com/example/app/ELearingAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ELearingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearingAppApplication.class, args);
+	}
+
+}
